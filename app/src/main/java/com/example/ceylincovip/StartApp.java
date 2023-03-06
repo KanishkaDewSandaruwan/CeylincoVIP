13dba63bde1b43cd6d3b1008daac5c73d8cd47cb
thilinimaheshika <kthilini1999@gmail.com>
2023-03-06 04:08:32 -0800

    Second New Changes made

--- app/src/main/java/com/example/ceylincovip/StartApp.java
+++ app/src/main/java/com/example/ceylincovip/StartApp.java
@@ -3,7 +3,9 @@ package com.example.ceylincovip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StartApp extends AppCompatActivity {
@@ -21,5 +23,19 @@ public class StartApp extends AppCompatActivity {
         f_ins = (Button) findViewById(R.id.full_in);
         t_party = (Button) findViewById(R.id.third_p);
         dealer = (Button) findViewById(R.id.buttondealer);
+
+        f_ins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Vehicle_Selection.class));
+            }
+        });
+
+        t_party.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Vehicle_Selection.class));
+            }
+        });
     }
 }
